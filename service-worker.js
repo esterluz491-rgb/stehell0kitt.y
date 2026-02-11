791e598f7f276fbe07380995aa467ac016aef51a
esterluz491-rgb <ester.luz491@gmail.com>
2026-02-11 16:21:05 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,11 @@ const urlsToCache = [
   // adiciona aqui outros assets que quiseres cachear, como CSS, JS e imagens
 ];
 
+// Evento de instalação
 self.addEventListener("install", event => {
+  // Forçar ativação imediata
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
@@ -14,10 +18,25 @@ self.addEventListener("install", event => {
   );
 });
 
+// Evento de ativação
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Intercepta requisições
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
-  );
-});
+
